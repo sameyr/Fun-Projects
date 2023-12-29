123df6c849794036b78ac44a3638d895f278d4ee
sameyr <ss1366t@gre.ac.uk>
2023-12-29 23:39:54 +0000

    All the pages added

--- date.js
+++ date.js
@@ -2,3 +2,18 @@ function redirectToYesPage(){
    window.location.href='yes.html';
 }
 
+function redirectToNoPage(){
+    window.location.href='no.html';
+}
+
+function shift(){
+    var noButton = document.querySelector('.answer:last-child');
+
+    // Calculate random positions (adjust the range as needed)
+    var randomLeft = Math.floor(Math.random() * window.innerWidth);
+    var randomTop = Math.floor(Math.random() * window.innerHeight);
+
+    // Set the random positions
+    noButton.style.left = randomLeft + 'px';
+    noButton.style.top = randomTop + 'px';
+}
